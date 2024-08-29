b1c764b10dba18523ecfc1ed3f2a6eb808687499
Juliana <138030471+juliana2023000000@users.noreply.github.com>
2024-08-29 01:29:41 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let anoAtual = 2025;
+const nomeCompleto = "Juliana de Lima Golinelli";
+let idade = 20;
+const cursoFacul = "Engenharia de Software";
+let cidadeAtual = "Sao paulo";
+
+console.log(`Hello I'm ${nomeCompleto}, I'm ${idade}, I live in ${cidadeAtual}, I wanna, next year, stury ${cursoFacul}, in ${anoAtual} I wanna buy my pirates games.`);
