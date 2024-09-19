7472efadb88c7206a9a7a347ab7d947ae562d8c2
Juliana <138030471+juliana2023000000@users.noreply.github.com>
2024-09-19 01:35:26 +0000

    18/09/24

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-let anoAtual = 2025;
-const nomeCompleto = "Juliana de Lima Golinelli";
-let idade = 20;
-const cursoFacul = "Engenharia de Software";
-let cidadeAtual = "Sao paulo";
+//let anoAtual = 2025;
+//const nomeCompleto = "Juliana de Lima Golinelli";
+//let idade = 20;
+//const cursoFacul = "Engenharia de Software";
+//let cidadeAtual = "Sao paulo";
 
-console.log(`Hello I'm ${nomeCompleto}, I'm ${idade}, I live in ${cidadeAtual}, I wanna, next year, stury ${cursoFacul}, in ${anoAtual} I wanna buy my pirates games.`);
+//console.log(`Hello I'm ${nomeCompleto}, I'm ${idade}, I live in ${cidadeAtual}, I wanna, next year, stury ${cursoFacul}, in ${anoAtual} I wanna buy my pirates games.`);
+
+//agencia de viagens
+const nomeComprador = "Souza";
+const CPF = 12012102102;
+const idadeComprador = 18;
+const listaDestinos = new Array(
+  "Japão",
+  " Canada",
+  " Mexico",
+  " França",
+  " China"
+);
+
+//console.log(`olá sr. ${nomeComprador}, do CPF ${CPF}, suas opções de destino são: ${listaDestinos}`);
+
+console.log(listaDestinos[0]);
+console.log();
+console.log(`Adicionamos um novo destino a sua lista`);
+
+listaDestinos.push("Londres");
+
+console.log(listaDestinos);
