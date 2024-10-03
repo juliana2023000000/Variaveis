33c8421cc9c08bf8accf0ebffd69b3a8540256df
Juliana <138030471+juliana2023000000@users.noreply.github.com>
2024-10-03 01:33:41 +0000

    02/10/24

--- index.js
+++ index.js
@@ -10,8 +10,29 @@ const entrada = prompt();
 //console.log(`Hello I'm ${nomeCompleto}, I'm ${idade}, I live in ${cidadeAtual}, I wanna, next year, stury ${cursoFacul}, in ${anoAtual} I wanna buy my pirates games.`);
 
 //agencia de viagens
+console.log(`----------------------`);
+console.log(`        LOGIN         `);
+console.log(`----------------------`);
+
+const loginCerto = "Alan3";
+const senhaCerta = 12345;
+
+var login = entrada("Digite seu login: ");
+var senha = entrada("Digite sua senha: ");
+
+while(login != loginCerto) {
+  console.log(`Login não encontrado.`);
+  login = entrada(`Digite novamente seu login: `);
+}
+
+while(senha != senhaCerta) {
+  console.log(`Senha não encontrada.`)
+  senha = entrada(`Digite novamente sua senha: `);
+}
+
+
 var nomeComprador = entrada("Qual seu nome? ");
-//const CPF = 12012102102;
+var CPF = entrada("Qual seu cfp? ");
 var idadeComprador = entrada("Qual sua idade? ");
 const listaDestinos = new Array(
   "Japão",
@@ -33,6 +54,7 @@ listaDestinos.push("Londres");*/
 if(idadeComprador < 18) {
   console.log(`infelizmente não podemos lhe vender uma passagem, pois o senhor(a) é menor de idade`);
 } 
+
 if(idadeComprador > 18) {
   console.log(`podemos lhe vender a passage, aqui estão os destinos ${listaDestinos}`)
 }
